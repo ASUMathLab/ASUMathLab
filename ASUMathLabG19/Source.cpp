fe18b0338c2ca7ffe2915fc10bb29760f2c47f7e
AhmedSadek60 <ahmed_sadek60@hotmail.com>
2017-10-28 15:29:27 +0200

    Add project files.

---
+++ ASUMathLabG19/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include<fstream>
+#include<string>
+#include "CMatrix.h"
+#include "stdarg.h"
+using namespace std;
+void main() {
+/*
+	CMatrix k;
+	k=CMatrix::CMatrix(3, 3, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2,3.0,3.1,3.2);
+//	double c=CMatrix::getDeterminant();
+	cout << k.getDeterminant(); 
+	*/
+
+
+	system("pause");
+}
