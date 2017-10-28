5821018fe4c5b5fdfb5b08b9964c2038c9170c44
ahmezzats <ahm.ezzat.s@gmail.com>
2017-10-28 19:23:25 +0200

    First version, Identify inputs and make operations on 2 matrices

--- ASUMathLabG19/Source.cpp
+++ ASUMathLabG19/Source.cpp
@@ -4,15 +4,101 @@
 #include<string>
 #include "CMatrix.h"
 #include "stdarg.h"
-using namespace std;
-void main() {
-/*
-	CMatrix k;
-	k=CMatrix::CMatrix(3, 3, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2,3.0,3.1,3.2);
-//	double c=CMatrix::getDeterminant();
-	cout << k.getDeterminant(); 
-	*/
-	 
+#include <map>
+#include "stdio.h"
 
-	system("pause");
+using namespace std;
+
+void deleteWhiteSpaces(string& input)
+{
+	int length = input.length();
+	for (int i = 0; i < length; i++) {
+		if (input[i] == ' ')
+		{
+			input.erase(i, 1);
+			length--;
+			i--;
+		}
+	}
 }
+
+void main() {
+
+	string input;
+	char key;
+
+	map<char, CMatrix> Matrix;
+
+	
+	
+	while (1)
+	{
+		getline(cin, input);
+
+		if (input.length() == 1)
+		{
+			key = input[0];
+			cout << Matrix[key] << endl;
+		}
+		else
+		{
+			if(input.find("[") != string::npos)
+			{
+			
+				int bracketOccurence = input.find("[");
+
+				CMatrix temp(input.substr(bracketOccurence, input.length()));
+
+				key = input[0];
+				Matrix [key] = temp;
+				
+				//in case of more than 1 ='s
+				
+				deleteWhiteSpaces(input);
+
+				for (int i = 0; i < bracketOccurence; i++) {
+					if (input[i] == '=')
+					{
+						key = input[i-1];
+						Matrix[key] = temp;
+					}
+				}
+
+				cout << Matrix[key];
+
+			}
+			else
+			{
+				deleteWhiteSpaces(input);
+				if (input.find("+") != string::npos)
+				{
+					CMatrix temp = Matrix[input[2]] + Matrix[input[4]];
+					key = input[0];
+					Matrix[key] = temp;
+					cout << Matrix[key];
+				}
+				else if ((input.find("-") != string::npos))
+				{
+					CMatrix temp = Matrix[input[2]] - Matrix[input[4]];
+					key = input[0];
+					Matrix[key] = temp;
+					cout << Matrix[key];
+				}
+				else if ((input.find("*") != string::npos))
+				{
+					CMatrix temp = Matrix[input[2]] * Matrix[input[4]];
+					key = input[0];
+					Matrix[key] = temp;
+					cout << Matrix[key];
+				}
+			}
+
+			
+		}
+
+	}
+
+}
+
+
+
